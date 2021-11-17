e25329aa0a6d7cdbcbcc27e0303f6571598b9be8
Todd Trulock <toddstrulock@gmail.com>
2021-11-17 08:49:01 -0600

    updated code.

--- db/Employee_Tracker.sql
+++ db/Employee_Tracker.sql
@@ -9,3 +9,21 @@ CREATE TABLE Department (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE Role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title  VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE Employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR  (30) NOT NULL,
+    role_id INTEGER (30) NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY(id)
+    FOREIGN KEY (role_id) REFERENCES Role (id)
+);
--- db/Seed.sql
+++ db/Seed.sql
@@ -1,3 +1,28 @@
 USE employee_tracker;
 
 INSERT INTO Department (name)
+    VALUES ('Sales'),
+           ('Engineering')
+           ('Finance'),
+           ('Legal')
+VALUES
+
+
+INSERT INTO Role
+    (title, salary, department_id)
+VALUES
+    ('Sales Manager', 110000, 1),
+    ('Sales Person', 75000, 1),
+
+    ('Mechanical Engineer', 120000, 2),
+    ('Software Engineer', 130000, 2),
+
+    ('Account Manager', 150000, 3),
+    ('Accountant', 115000,3),
+
+    ('Legal Advisor Lead', 180000,4),
+    ('Lawyer', 165000, 4);
+
+VALUES
+('Jacob', 'Butler',1, NULL),
+('Kalkhi', 'Y Chili')
